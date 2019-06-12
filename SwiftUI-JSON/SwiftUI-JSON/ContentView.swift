2616576774aac28cac47a077555f224d9ef57be4
iMakca <ukr-maksim1@yandex.ru>
2019-06-12 20:06:59 +0300

    add project

---
+++ SwiftUI-JSON/SwiftUI-JSON/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-JSON
+//
+//  Created by Maxim Granchenko on 6/12/19.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
